a2b6b1da9dfe2debae432f2d61875dce5f87e15f
Grant <53729381+endgrant@users.noreply.github.com>
2023-12-27 12:21:30 -0500

    Added Player

---
+++ Entity/KinematicEntity/KinematicEntity.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+public partial class KinematicEntity : CharacterBody2D
+{
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _PhysicsProcess(double delta) {
+	}
+}
